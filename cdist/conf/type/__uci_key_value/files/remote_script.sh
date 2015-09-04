31194b3141ebc7d373c3c55f33255b8e8cd2d663
Jade Philipoom <jadep@mit.edu>
2015-09-04 12:39:51 -0400

    First tested and working version.

--- cdist/conf/type/__uci_key_value/files/remote_script.sh
+++ cdist/conf/type/__uci_key_value/files/remote_script.sh
@@ -1,16 +1,14 @@
+export id="$(cat "$__object/parameter/config" 2>/dev/null \
+   || echo "$__object_id")"
 export config="$(cat "$__object/parameter/config")"
 export section="$(cat "$__object/parameter/section")"
 export option="$(cat "$__object/parameter/option")"
 export value="$(cat "$__object/parameter/value")"
 export state="$(cat "$__object/parameter/state")"
 
-if ["$state" == "absent"]; then
-	uci delete "$config"."$section"."$option"
+if [ "$state" == "absent" ]; then
+	uci delete "$config.$section.$option"
 else
-	export current="$(echo uci get "$config"."$section"."$option")"
-	if ["$current" != "$value"]; then
-		uci set "$config"."$section"."$option"="$value"
-                echo "change" >> "$__messages_out"
-		uci commit "$config"
-	fi
+	uci set "$config.$section.$option=$value"
 fi
+uci commit "$config"
